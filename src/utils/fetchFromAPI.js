972e3d1e39e5c7cc8c31b59ef104382773f9e743
Debjit <beradebjit3@gmail.com>
2023-09-20 03:07:19 +0530

    Initial Commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '6379ab014fmshaaafb7c9e13aa33p1d5932jsn96785ed836a5',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
